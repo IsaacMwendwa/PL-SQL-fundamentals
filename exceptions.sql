b5aa5c0311eac28f5e8c61fad429a366cf67b9da
Isaac Mwendwa <isaacmwendwa107@gmail.com>
2021-07-12 20:46:22 +0300

    Update exceptions.sql

--- exceptions.sql
+++ exceptions.sql
@@ -9,18 +9,3 @@ BEGIN
     -- display customer name
     DBMS_OUTPUT.PUT_LINE('Customer name is: ' || l_name);
 END;
-
-DECLARE
-    l_name customers.NAME%TYPE;
-    l_customer_id customers.customer_id%TYPE := &customer_id;
-BEGIN
-    -- get the customer name by id
-    SELECT name INTO l_name
-    FROM customers
-    WHERE customer_id = l_customer_id;
-
-    -- show the customer name   
-    dbms_output.put_line('Customer name is ' || l_name);
-    
-END;
-/
