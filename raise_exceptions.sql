264468291659b3654dd8f4740c86c34ca0f06c06
Isaac Mwendwa <isaacmwendwa107@gmail.com>
2021-07-12 21:43:43 +0300

    Raising user and internally defined exceptions in PL/SQL

--- raise_exceptions.sql
+++ raise_exceptions.sql
@@ -1,6 +1,6 @@
 SET SERVEROUTPUT ON;
 
--- User-defined exception
+-- Raising User-defined exception
 DECLARE
     e_credit_too_high EXCEPTION;
     PRAGMA exception_init( e_credit_too_high, -20001 );
@@ -26,6 +26,52 @@ BEGIN
     COMMIT;
 END;
 
--- internally defined exception
+-- Raising internally defined exception
 DECLARE
-    l_cu
+    l_customer_id customers.customer_id%TYPE := &customer_id;
+BEGIN
+    IF l_customer_id < 0 THEN
+        RAISE invalid_number;
+    END IF;
+END;
+
+-- Re-raising current exception
+DECLARE
+    e_credit_too_high EXCEPTION;
+    PRAGMA exception_init( e_credit_too_high, -20001 );
+    l_max_credit customers.credit_limit%TYPE;
+    l_customer_id customers.customer_id%TYPE := &customer_id;
+    l_credit customers.credit_limit%TYPE     := &credit_limit;
+BEGIN
+    BEGIN
+        -- get the max credit limit
+        SELECT MAX(credit_limit) 
+        INTO l_max_credit
+        FROM customers;
+        
+        -- check if input credit is greater than the max credit
+        IF l_credit > l_max_credit THEN 
+            RAISE e_credit_too_high;
+        END IF;
+        EXCEPTION
+            WHEN e_credit_too_high THEN
+                dbms_output.put_line('The credit is too high' || l_credit);
+                RAISE; -- reraise the exception
+    END;
+EXCEPTION
+    WHEN e_credit_too_high THEN
+        -- get average credit limit
+        SELECT avg(credit_limit) 
+        into l_credit
+        from customers;
+        
+        -- adjust the credit limit to the average
+        dbms_output.put_line('Adjusted credit to ' || l_credit);
+    
+        --  update credit limit
+        UPDATE customers 
+        SET credit_limit = l_credit
+        WHERE customer_id = l_customer_id;
+   
+        COMMIT;
+END;
