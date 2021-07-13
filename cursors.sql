3a99630c784a165f8fe982cfc400e707018257f3
Isaac Mwendwa <isaacmwendwa107@gmail.com>
2021-07-13 18:24:57 +0300

    Cursors in PL/SQL

--- cursors.sql
+++ cursors.sql
@@ -1,5 +1,15 @@
 SET SERVEROUTPUT ON;
 
+-- Create view returning sales revenues by customers
+CREATE VIEW sales AS
+    SELECT customer_id, SUM(unit_price * quantity) total,
+        ROUND(SUM(unit_price * quantity) * 0.05) credit
+    FROM order_items INNER JOIN orders USING (order_id)
+    WHERE status = 'Shipped'
+    GROUP BY customer_id;
+
+SELECT * FROM SALES;
+
 /* create anonymous block that
 -- resets credit limits of all customers to zero
 -- fetches customers sorted by sales in descending ordern & gives them new credit limits from a budget of 1million
@@ -20,12 +30,12 @@ BEGIN
     OPEN c_sales;
     LOOP
         FETCH  c_sales INTO r_sales;
-        EXIT WHEN c_sales%NOTFOUND --no rows remaining
+        EXIT WHEN c_sales%NOTFOUND; --no rows remaining
         
         --update credit for current customer
         UPDATE customers SET credit_limit =
-            CASE WHEN l_budget > r_sales.credit
-                THEN r_sales.credit ELSE l_budget
+            CASE WHEN l_budget > r_sales.credit THEN r_sales.credit 
+                ELSE l_budget
             END
             WHERE customer_id = r_sales.customer_id;
             
@@ -40,3 +50,7 @@ BEGIN
     END LOOP;
     CLOSE c_sales;
 END;
+
+--Verify update
+SELECT customer_id, name, credit_limit FROM customers ORDER BY credit_limit DESC;
+SELECT SUM(CREDIT_LIMIT) FROM customers;
