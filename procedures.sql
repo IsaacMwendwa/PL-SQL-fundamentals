b9935ae9c16f94d85ad921ccf21cb8f658cc92af
Isaac Mwendwa <isaacmwendwa107@gmail.com>
2021-07-14 13:30:17 +0300

    Stored Procedures in PL/SQL

--- procedures.sql
+++ procedures.sql
@@ -1,11 +1,11 @@
 -- procedure to print customer's contact info
 
-CREATE OR REPLACE PROCEDURE print_contact( p_customer_id NUMBER)
+CREATE OR REPLACE PROCEDURE print_contact( in_customer_id NUMBER)
 IS r_contact contacts%ROWTYPE;
 
 BEGIN
-    -- get contaact based on customer id
-    SELECT * INTO r_contact FROM contacts WHERE customer_id = p_customer_id;
+    -- get contact based on customer id
+    SELECT * INTO r_contact FROM contacts WHERE customer_id = in_customer_id;
     
     -- print contacts info
     DBMS_OUTPUT.PUT_LINE( r_contact.first_name || ' ' || r_contact.last_name || '<' || r_contact.email || '>' );
@@ -14,3 +14,6 @@ BEGIN
         WHEN OTHERS THEN
             DBMS_OUTPUT.PUT_LINE( SQLERRM);
 END;
+
+-- execute procedure
+EXECUTE print_contact(100);
